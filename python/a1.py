63fb95a3854ec44adcc7b80f057f2baf65e50025
kanagarajm496 <kanagarajm496@gmail.com>
2026-07-02 10:49:23 +0530

    Day 01 - HTML Basics and Python Basics Completed

---
+++ python/a1.py
@@ -0,0 +1,9 @@
+print("hello,logesh")
+print("I am D.Logesh")
+print("I am learning python full stck development")
+name="logesh"
+print(name)
+mark=99
+print(mark)
+age=21
+print(age)
---
+++ python/a2.py
@@ -0,0 +1,4 @@
+name=input("enter the name:")
+age=input("enter the age:")
+print("name:",name)
+print("age:",age)
